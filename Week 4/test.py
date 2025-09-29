bea2bafd056fef75d6126952d4ac58f025d8584b
Nathan Wilson <a02300182@aggies.usu.edu>
2025-09-28 20:39:09 -0600

    Mostly done

---
+++ Week 4/test.py
@@ -0,0 +1,13 @@
+
+
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+corpus = [
+    "This is the first document.",
+    "This document is the second document.",
+    "And this is the third one.",
+    "Is this the first USU document?",
+]
+
+
